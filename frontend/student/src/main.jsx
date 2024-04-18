43f96017e3d7d25c4a14e7650f1aed34a4a1541c
sahishnu1123 <sahishnusahi@gmail.com>
2024-04-18 20:20:11 +0530

    merge rebase conflict

--- frontend/student/src/main.jsx
+++ frontend/student/src/main.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import routes from "./routes";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Header from "./components/Header"
+// import './index.css'
+// import routes from "./routes";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import Header from "./components/Header"
+import Courseregpage from "./components/course_reg.jsx"
 
-const router = createBrowserRouter(routes);
+// const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Header/>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <Header/>
+  //   <RouterProvider router={router} />
+  // </React.StrictMode>
+  <Courseregpage/>
 );
 
