862589b013e66126af736db22dd171ad1e7ef6a0
test <rutheeshreddychittepu@gmail.com>
2024-04-19 10:43:18 +0530

    somme code

--- frontend/student/src/components/Homepage.jsx
+++ frontend/student/src/components/Homepage.jsx
@@ -1,4 +1,4 @@
-//import StudentInfo from "./StudentInfo"
+import StudentInfo from "./StudentInfo"
 import { useEffect } from "react"
 const Homepage = () => 
 {   let studinfo;
@@ -8,34 +8,14 @@ const Homepage = () =>
          token = sessionStorage.getItem("token") 
          //verify token
          axios.post(import.meta.env.VITE_STUDENT, {
-            userName: user,
-            password: pwd,
-            }).then((res) => {
-            
-            if (res.data.logRes == 1) {
-          
-                //sessionStorage.setItem('token', res.data.token);
-                //localStorage.setItem('token', res.data.token);
-                //console.log('token:',res.data.token)
-             setSucc(true) 
-            setTimeout(() => {
-
-                // navigate  
-                window.location.href = import.meta.env.VITE_STUDENT + "redirect/"+res.data.token // temporary workaround, shall fix later
-                //setSucc(false)
-            }, 1000);
-
-
+            headers: {
+                'Content-Type': "application/json",
+                'Authorization': `Bearer ${token}`,
             }
+        }).then((res) => {
             
-            else if (res.data.logRes == -1) // invalid userame
-            {
-              userRef.current.focus();  setPwd(""); setErrMsg("This username doesn't exist")
-            }
-
-            else //wrong password
-            {
-              setPwd(""); setErrMsg("The password is wrong, try again")
+            if (res.data.tokenStatus != 1) {
+                window.location.href = import.meta.env.VITE_LOGIN
             }
 
             }).catch((err) => {
@@ -49,8 +29,8 @@ const Homepage = () =>
 
 
     return (<>
-    Andhra Bhoja!
-    <button onClick = {()=>{console.log(localStorage.getItem("token"))}}>raja</button>
+               <StudentInfo/>
+
     </>
 
     )
