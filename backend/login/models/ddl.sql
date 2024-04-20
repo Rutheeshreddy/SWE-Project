5ca9aee02babd4dbb87eae761c6802eb1d1245cb
Sasank-2004 <cs21btech11019@iith.ac.in>
2024-04-20 16:31:37 +0530

    updated schema

--- backend/login/models/ddl.sql
+++ backend/login/models/ddl.sql
@@ -1,45 +1,44 @@
-drop table if exists login
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+
 create table  login ( 
 	username varchar (30) primary key,
 	password varchar(30) NOT NULL,
 	role varchar(15) NOT NULL ) ; 
 
-drop table if exists student
 create table  student (
-	id varchar(20) primary key,
-	name varchar(20) NOT NULL,
-	department varchar(10) NOT NULL 
+	id varchar(30) primary key,
+	name varchar(30) NOT NULL,
+	department varchar(10) NOT NULL,
+	joining_year integer NOT NULL 
 );
 
-drop table if exists instructor
 create table  instructor (
-	id varchar(20) primary key,
-	name varchar(20) NOT NULL,
+	id varchar(30) primary key,
+	name varchar(30) NOT NULL,
 	department varchar(10) NOT NULL
 );
 	
-drop table if exists past_courses
 create table  past_courses (
 	course_id varchar(10),
 	semester varchar(10),
 	year integer,
 	name varchar(30) NOT NULL,
 	credits integer default 0,
-	instructor_id varchar(20) ,
+	instructor_id varchar(30) ,
 	prerequisites varchar(50),
 	max_capacity integer,
 	primary key (course_id,semester,year),
 	foreign key (instructor_id) references instructor on delete cascade
 );
 
-drop table if exists present_courses
 create table  present_courses (
 	course_id varchar(10),
 	semester varchar(10),
 	year integer,
 	name varchar(30) NOT NULL,
 	credits integer default 0,
-	instructor_id varchar(20) ,
+	instructor_id varchar(30),
 	prerequisites varchar(50),
 	slot char,
 	max_capacity integer,
@@ -47,23 +46,36 @@ create table  present_courses (
 	foreign key (instructor_id) references instructor on delete cascade
 );
 
-drop table if exists student_courses
-create table  student_courses(
+create table  student_courses_past(
 	course_id varchar(10),
 	semester varchar(10),
 	year integer,
 	name varchar(30) NOT NULL,
 	credits integer default 0,
-	student_id varchar(20),
+	elective varchar(20),
+	student_id varchar(30),
 	grade varchar(5) ,
 	feedback varchar(200) ,
 	primary key (course_id,semester,year,student_id),
 	foreign key (student_id) references student on delete cascade,
-	foreign key (course_id,semester,year) references past_courses on delete cascade,
+	foreign key (course_id,semester,year) references past_courses on delete cascade
+);
+
+create table  student_courses_present(
+	course_id varchar(10),
+	semester varchar(10),
+	year integer,
+	name varchar(30) NOT NULL,
+	credits integer default 0,
+	elective varchar(20),
+	student_id varchar(30),
+	grade varchar(5) ,
+	feedback varchar(200) ,
+	primary key (course_id,semester,year,student_id),
+	foreign key (student_id) references student on delete cascade,
 	foreign key (course_id,semester,year) references present_courses on delete cascade
 );
 
-drop table if exists proposed_courses
 create table  proposed_courses(
 	course_id varchar(10),
 	name varchar(30) NOT NULL,
@@ -72,16 +84,21 @@ create table  proposed_courses(
 	primary key (course_id)
 );
 
-drop table if exists selected_courses
 create table  selected_courses (
 	course_id varchar(10),
-	teacher_id varchar(20)
+	teacher_id varchar(30),
+	teacher_selected integer DEFAULT 0,
+	primary key (course_id,teacher_id)
 );
 
-drop table if exists timeline
 create table  timeline (
 	course_selection integer default 0,
 	course_reg integer default 0,
 	course_feedback integer default 0,
-	course_gradeing integer default 0
+	course_grading integer default 0
+);
+
+create table current_sem (
+	semester varchar(10),
+	year integer 
 );
