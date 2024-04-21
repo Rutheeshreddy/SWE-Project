907d4f17af132e8466230f85500a5f7b8630e233
sahishnu1123 <sahishnusahi@gmail.com>
2024-04-21 15:05:38 +0530

    course registration page

--- frontend/student/src/main.jsx
+++ frontend/student/src/main.jsx
@@ -4,19 +4,19 @@ import './index.css'
 // import routes from "./routes";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // import Header from "./components/Header"
-import Courseregpage from "./components/course_reg.jsx"
+import Courseregpage from "./components/CourseReg.jsx"
 
 // const router = createBrowserRouter(routes);
 
-const sample1 = [
-  { id: 1, Course_ID: 'CS101', Instructor: 'John Doe', Credits: 3, Semester: 'Spring 2024', Elective: 'No' },
-  { id: 2, Course_ID: 'ENG201', Instructor: 'Jane Smith', Credits: 4, Semester: 'Fall 2024', Elective: 'Yes' }
-]
+// const reg = [
+//   { id: 1, course_id: 'CS101', coursename:'Intro to Computers', instructor: 'John Doe', credits: 3, semester: 'Spring 2024', slot: 'N', current : [150, 200], elective: 'Free'},
+//   { id: 2, course_id: 'ENG201', coursename: 'English Grammar', instructor: 'Jane Smith', credits: 4, semester: 'Fall 2024', slot: 'Z', current: [31, 49], elective: 'Departmental'}
+// ]
 
-const sample2 = [
-  { id: 1, Course_ID: 'CS111', Instructor: 'John Villa', Credits: 3, Semester: 'Spring 2024', Elective: 'No' },
-  { id: 2, Course_ID: 'ENG201', Instructor: 'Jane Smith', Credits: 4, Semester: 'Fall 2024', Elective: 'Yes' }
-]
+// const aval = [
+//   { id: 1, course_id: 'CS111', coursename: 'Comp Sci 2', instructor: 'John Villa', credits: 3, semester: 'Spring 2024', slot: 'J', current: [16, 100], elective: null},
+//   { id: 2, course_id: 'ENG281', coursename: 'English Intro', instructor: 'Jane Smith', credits: 4, semester: 'Fall 2024', slot: 'K', current: [25, 50], elective: null}
+// ]
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
@@ -24,6 +24,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   //   <RouterProvider router={router} />
   // </React.StrictMode>
 
-  <Courseregpage regCourses = {sample1} avalCourses = {sample2}/>
+  <Courseregpage regCourses = {reg} avalCourses = {aval}/>
 );
 
