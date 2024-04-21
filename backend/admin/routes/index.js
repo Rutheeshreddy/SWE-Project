9a0eb0d694ae76d74c997345636197b3fa6d1164
Sasank-2004 <cs21btech11019@iith.ac.in>
2024-04-21 15:09:18 +0530

    added current_period

--- backend/admin/routes/index.js
+++ backend/admin/routes/index.js
@@ -42,29 +42,48 @@ router.get('/test',authenticateToken,(req,res)=>
 
 router.get('/verify',authenticateToken, async (req,res) => 
 {
-  const query = {
-    name: 'get-sem-details',
-    text: 'SELECT * FROM current_sem'
-  }
 
   var current_sem = "";
   var current_year = 0;
+  var current_period = "";
 
   try {
+    const query = {
+      name: 'get-sem-details',
+      text: 'SELECT * FROM current_sem'
+    }
     const res1 = await client.query(query);
     current_sem = res1.rows[0].semester;
     current_year = res1.rows[0].year;
   }
   catch(err) {
-    console.log(err.stack)
+    console.log(err.stack);
+  }
+
+  try {
+    const query = {
+      name: 'get-period-details',
+      text: 'SELECT * FROM timeline'
+    }
+    const res1 = await client.query(query);
+    if(res1.rows[0].course_selection == 1) current_period = 'course_selection';
+    if(res1.rows[0].course_reg == 1) current_period = 'course_reg';
+    if(res1.rows[0].course_feedback == 1) current_period = 'course_feedback';
+    if(res1.rows[0].course_grading == 1) current_period = 'course_grading';
+
+  }
+  catch(err) {
+    console.log(err.stack);
   }
   
   res.json({
     tokenStatus : 1,
     sem : current_sem,
     year : current_year,
-    name : req.user.userName
+    name : req.user.userName.split('@')[0],
+    period : current_period
   })
+
 
 })
 
