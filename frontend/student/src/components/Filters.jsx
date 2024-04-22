ca31b751800c4d62249350e934818ac618734639
sahishnu1123 <sahishnusahi@gmail.com>
2024-04-22 15:42:23 +0530

    updated filters course registration

--- frontend/student/src/components/Filters.jsx
+++ frontend/student/src/components/Filters.jsx
@@ -1,24 +1,21 @@
 import React, { useEffect, useState, useRef } from "react";
 
-function Filters(){
+function Filters(props){
 
-    const [filters, setFilters] = useState({
-        courseId: "",
-        courseName: "",
-        instructor: "",
-        semester: "",
-        slot: "",
-        credits: "",
-        currentStudents: "",
-      });
+    const filters = props.filters
+    const updatefilters = props.updatefilters
     
-      const handleChange = (e, field) => {
-        setFilters({ ...filters, [field]: e.target.value });
-      }
+    const handleChange = (e, field) => {
+      updatefilters({ ...filters, [field]: e.target.value });
+    }
+
+    const HandleFilters = (filters_) => {
+
+    }
 
     return(
 
-        <div className="grid grid-cols-8 justify-between font-semibold items-center mb-1">
+        <div className="grid grid-cols-9 justify-between font-semibold items-center mb-1">
                 
                 <div>
                   <input className = "w-24 h-6"
@@ -33,6 +30,14 @@ function Filters(){
                     type="text"
                     placeholder="Filter..."
                     value={filters.courseName}
+                    onChange={(e) => handleChange(e, "courseName")}
+                  />
+                </div>
+                <div>
+                  <input className = "w-24 h-6"
+                    type="text"
+                    placeholder="Filter..."
+                    value={filters.department}
                     onChange={(e) => handleChange(e, "courseName")}
                   />
                 </div>
@@ -69,14 +74,14 @@ function Filters(){
                   />
                 </div>
                 <div>
-                  <input className = "w-24 h-6"
+                  {/* <input className = "w-24 h-6"
                     type="text"
                     placeholder="Filter..."
                     value={filters.currentStudents}
                     onChange={(e) => handleChange(e, "currentStudents")}
-                  />
+                  /> */}
                 </div>
-                <div><button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Search</button></div>
+                <div><button onClick={() => HandleFilters(filters)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Search</button></div>
               </div>  
     )
 }
