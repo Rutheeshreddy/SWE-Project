ae48a22714f084b429f68eb42dc093b67e86c808
sahishnu1123 <sahishnusahi@gmail.com>
2024-04-26 17:20:57 +0530

    commiting to pull for backend connection student page

--- frontend/student/src/components/Homepage.jsx
+++ frontend/student/src/components/Homepage.jsx
@@ -7,32 +7,46 @@ import axios from "axios"
 
 const Homepage = () => 
 {   //let studinfo;
-//     let regcourses; let token;
-//     useEffect(()=>
-//     {    
-//          token = sessionStorage.getItem("token") 
-//          //verify token
-//          axios.get(import.meta.env.VITE_STUDENT+"verify", {
-//             headers: {
-//                 'Content-Type': "application/json",
-//                 'Authorization': `Bearer ${token}`,
-//             }
-//         }).then((res) => {
-            
-//             if (res.data.tokenStatus != 1) {
-//                 window.location.href = import.meta.env.VITE_LOGIN
-//             }
-//             console.log("Ok, verify is working")
-//             //student info, reg courses
-            
-//             }).catch((err) => {
-            
-//             console.log(err);
-//             console.log("There is some problem with the server or your internet, try again after some time")
-//             })
-//            },[])
+    let regcourses; let token;
+    const [details, setdetails] = useState([])
+    useEffect(()=>
+    {    
+         token = sessionStorage.getItem("token") 
+         //verify token
+         axios.get(import.meta.env.VITE_STUDENT+"verify", {
+            headers: {
+                'Content-Type': "application/json",
+                'Authorization': `Bearer ${token}`,
+            }
+        }).then((res) => {
 
-       const details = {"name": "Raja", "id": "CS21BTECH11000", "department": "Computer Science and Engineering", "batch": "2021", "semester": "6"}
+              console.log(res.data)
+            
+            if (res.data.tokenStatus != 1) {
+                window.location.href = import.meta.env.VITE_LOGIN
+            }
+            console.log("Ok, verify is working")
+
+            if(res.data.status === 1){
+
+              detailstemp = {name: res.data.student.name, id: res.data.student.id, department: res.data.student.department, batch : res.data.student.joining_year, semester: res.data.sem.semester}
+              setdetails(detailstemp)
+
+            }
+            
+            }).catch((err) => {
+            
+            console.log(err);
+            console.log("There is some problem with the server or your internet, try again after some time")
+            })
+           },[])
+
+       useEffect(()=>{
+
+              console.log(details)
+       },[details])
+
+       // const details = {"name": "Raja", "id": "CS21BTECH11000", "department": "Computer Science and Engineering", "batch": "2021", "semester": "6"}
 
        const r1 = {"courseName":"Deep Learning", "courseCode":"AI1100", "electiveType":"Departmental"}
        const r2 = {"courseName":"Machine Learning", "courseCode":"AI1000", "electiveType":"Additional"}
--- frontend/student/src/main.jsx
+++ frontend/student/src/main.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-// import routes from "./routes";
+import routes from "./routes";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import Header from "./components/Header"
-import Courseregpage from "./components/CourseReg.jsx"
+import Header from "./components/Header"
 
-// const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  //   <Header/>
-  //   <RouterProvider router={router} />
-  // </React.StrictMode>
-
-  <Courseregpage/>
+  <React.StrictMode>
+    <Header/>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
 
