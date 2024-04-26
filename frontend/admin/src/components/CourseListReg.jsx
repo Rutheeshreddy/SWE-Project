fa1d7ba78e0da4ee69a8d3387ce8fa3972b0ee59
sahishnu1123 <sahishnusahi@gmail.com>
2024-04-26 11:12:56 +0530

    showing list of courses to the admin.

--- frontend/admin/src/components/CourseListReg.jsx
+++ frontend/admin/src/components/CourseListReg.jsx
@@ -17,6 +17,59 @@ function CourseListReg(){
         setFilters(filters)
     }
 
+    const [avalCourses, setAvalCourses] = useState([])
+    const [totPageNum, setTotPageNum] = useState(0)
+    const [pageNum, setPageNum] = useState(1)
+    const [temp, setTemp] = useState(1)
+
+    useEffect(() => {
+        setTemp(pageNum);
+        var token = sessionStorage.getItem("token");
+        axios.get(import.meta.env.VITE_ADMIN+"//" + pageNum ,{
+            headers: {
+                'Content-Type': "application/json",
+                'Authorization': `Bearer ${token}`,
+          }
+        }).then( (res) => {
+    
+            setTotPageNum(res.data.totPageNum)
+            setAvalCourses(res.data.courses)    
+        }).catch((err) => {            
+
+            console.log(err);
+            setErrMsg("There is some problem with the server or your internet, try again after some time")
+            })
+        
+      }, [pageNum]);
+
+      const handleprev = () =>{
+
+        if(pageNum > 1) setPageNum(pageNum - 1);
+      }
+
+      const handlepgno = () =>{
+
+        if(temp >= 1 && temp <= totPageNum)
+            setPageNum(displayNum);
+
+        else {
+            setPageNum(totPageNum);
+            setTemp(totPageNum);
+        }        
+                
+      }
+
+      const handlenext = () =>{
+
+        if(pageNum < totPageNum) setPageNum(pageNum + 1);
+        
+      }
+
+      const handleCourseIdClick = () =>{
+
+
+      }
+
 
     return(
 
@@ -60,12 +113,28 @@ function CourseListReg(){
 
                         </div>
                     </div>
+                    <div className="flex gap-3 justify-center font-semibold mb-6">
+                        <div> <button className="bg-blue-500 text-white px-2 py-1 rounded-md text-sm" onClick={()=>handleprev()}>prev</button> </div>
+
+                        <div className="flex flex-row items-center">
+            
+                            <input className="w-8 border-2" type="text" placeholder={pageNum}
+                            onKeyDown={(e) => {
+                            if (e.key === 'Enter') {
+                            handlepgno();
+                            }
+                            }}/>
+
+                            <div className="mx-2">of</div>
+                            <div>{totPageNumaval}</div>
+                        </div>
+
+                        <div> <button className="bg-blue-500 text-white px-2 py-1 rounded-md text-sm" onClick={()=>handlenext()}>next</button> </div>
+                    </div>
                 </div>
             </div>
         </div>
-    )
-
-    
+    )    
 }
 
 export default CourseListReg;
